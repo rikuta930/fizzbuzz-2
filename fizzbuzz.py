2062068196797624e1ab65c970015d43f75aec53
rimurun <rimuru930@gmail.com>
2020-08-19 03:38:44 +0900

    Amendの実験

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,15 +1,14 @@
 def fizzbuzz_convert(num):
     if num % 3 == 0 and num % 5 == 0:
         return "FizzBuzz"
-    elif num % 3 == 0:
+    if num % 3 == 0:
         return "Fizz"
-    elif num % 5 == 0:
+    if num % 5 == 0:
         return "Buzz"
-    else:
-        return str(num)
+    return str(num)
 
 
 # num = int(input('1つの自然数を入れてください:'))
-
+# Amend
 r = fizzbuzz_convert(1)
 print(r)
